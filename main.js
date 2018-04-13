066fc5e299f9c467f87a50d245048283d9b98bdc
mariana <m.nyshpor@gmail.com>
2018-04-13 06:59:09 +0300

    new file

--- main.js
+++ main.js
@@ -1,52 +1,50 @@
 class Sequence {
-  constructor(number, a1) {
-   	this.number = number;
-   	this.a1 = a1;
-   }
+    constructor(number, a1) {
+        this.number = number;
+        this.a1 = a1;
+    }
 
-   get multipleNumbers() {
-   	 let quantity = 0;
-     for (let i = this.a1; i <= this.number; i++) {
-       if (i % 3 === 0 && i % 5 !== 0) {
-    	   quantity++;
-       }
-     }
-     return quantity;
-   }
+    get multipleNumbers() {
+        let quantity = 0;
+        for (let i = this.a1; i <= this.number; i++) {
+            if (i % 3 === 0 && i % 5 !== 0) {
+                quantity++;
+            }
+        }
+        return quantity;
+    }
 
-   get evenNumbers() {
-   	 let quantity = 0;
-     for (let i = this.a1; i <= this.number; i++) {
-       if (Math.sqrt(i) % 2 === 0) {
-         quantity++;
-       }
-     }
-     return quantity;
-   }
+    get evenNumbers() {
+        let quantity = 0;
+        for (let i = this.a1; i <= this.number; i++) {
+            if (Math.sqrt(i) % 2 === 0) {
+                quantity++;
+            }
+        }
+        return quantity;
+    }
 
-   get pythagoreanTriple() {
- 	   const arr = [];
-	   for (let a = 0; a <= this.number; a++) {
+    get pythagoreanTriple() {
+        const arr = [];
+        for (let a = 0; a <= this.number; a++) {
 
-		   for (let b = a + 1; b <= this.number; b++) {
+            for (let b = a + 1; b <= this.number; b++) {
 
-		     for (let c = b + 1; c <= this.number; c++ ){
+                for (let c = b + 1; c <= this.number; c++) {
 
-			     if (a*a + b*b === c*c) {
+                    if (a * a + b * b === c * c) {
 
-			       let tripleString = `${a}, ${b}, ${c}`;
-			       arr.push(tripleString);
-			     }
-		     }
-		   }
-	   }
-	   return arr;
- 	 } 
- }
+                        let tripleString = `${a}, ${b}, ${c}`;
+                        arr.push(tripleString);
+                    }
+                }
+            }
+        }
+        return arr;
+    }
+}
 
- let sequence = new Sequence(30, 1);
- console.log(sequence.multipleNumbers);
- console.log(sequence.evenNumbers);
- console.log(sequence.pythagoreanTriple);
-
- 
+let sequence = new Sequence(30,1);
+console.log(sequence.multipleNumbers);
+console.log(sequence.evenNumbers);
+console.log(sequence.pythagoreanTriple);
